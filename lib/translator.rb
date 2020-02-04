7665df7ff69f07b95235aa3e334ba7df14d4eb13
Spencer Blum <spencerblum@gmail.com>
2020-02-04 21:24:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,7 @@ def get_japanese_emoticon(file_path, emoticon)
     result
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
   result = load_library(file_path)['get_meaning'][emoticon]
      if !result
